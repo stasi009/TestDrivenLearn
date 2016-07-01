58f79eaa59784a755b957a6b58c8a994c0bde372
Chuanlin Zhao <cheka008@gmail.com>
2016-06-30 17:38:21 -0700

    finish the first version of crawler of airbnb

--- Python/PlayWeb/TryCrawling/CrawlAirbnb/CrawlAirbnb.py
+++ Python/PlayWeb/TryCrawling/CrawlAirbnb/CrawlAirbnb.py
@@ -1,18 +1,16 @@
-
+﻿
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
 
-room_url = "https://www.airbnb.com/rooms/10705179"
-request = requests.get(room_url)
-soup = BeautifulSoup(request.content)
+import entities
 
-title = soup.find('h1',{"id":"listing_name"})
 
-reviews = soup.findAll('div',{'class':'row review'})
-reviews
 
-url = "https://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&listing_id=9460&role=all&_limit=2&_offset=2"
-request = requests.get(url)
-request.content
-d = json.loads(request.content)
+
+
+
+
+
+
---
+++ Python/PlayWeb/TryCrawling/CrawlAirbnb/abn_parser.py
@@ -0,0 +1,75 @@
+﻿
+
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+import itertools
+from abn_entities import Room
+
+Aspects = ["Accuracy","Communication","Cleanliness","Location","Check In","Value"]
+AspectPatterns = dict((aspect,re.compile(r'\${}.0.0.0$'.format(aspect))) for aspect in Aspects)
+
+def get_aspect_rating(soup,aspect,listingid):
+    pattern = AspectPatterns[aspect]
+    tags = soup.findAll('div',{'class':'star-rating','data-reactid':pattern})
+    if len(tags) == 1:
+        return float(tags[0].attrs["content"])
+    elif len(tags) == 0: # not found
+        return None
+    else:
+        errmsg = "Room<{}> has {} ratings for '{}'".format(listingid,len(tags),aspect)
+        raise Exception(errmsg)
+
+def get_saved2wishlist(soup,listingid):
+    tags = soup.findAll('div',{'class':'wish_list_button'})
+    if len(tags) == 1:
+        return int(tags[0].attrs['data-count'])
+    elif len(tags) == 0: # not found
+        return None
+    else:
+        errmsg = "Room<{}> has {} 'saved to wishlist'".format(listingid,len(tags))
+        raise Exception(errmsg)
+
+def parse_aspect_ratings(room):
+    url = "https://www.airbnb.com/rooms/{}".format(room.id)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content)
+
+    room.aspect_ratings = dict((aspect, get_aspect_rating(soup,aspect,room.id))  for aspect in Aspects)
+    room.saved2wishlist = get_saved2wishlist(soup,room.id)
+
+def is_english(txt):
+    try:
+        txt.decode('ascii')
+    except UnicodeEncodeError:
+        return False
+    else:
+        return True
+
+def parse_comments(listingid):
+    api_url = "https://api.airbnb.com/v2/reviews?client_id=3092nxybyb0otqw18e8nh5nty&listing_id={}&role=all".format(listingid)
+
+    response = requests.get(api_url)
+    # if there is no reviews, response still have 'reviews' section, but just empty
+    reviews = json.loads(response.content)["reviews"]
+
+    return [review["comments"] for review in reviews if is_english(review["comments"])]
+
+def parse_room(listing_id):
+    #################### basic information
+    api_url = "https://api.airbnb.com/v2/listings/{}?client_id=3092nxybyb0otqw18e8nh5nty&_format=v1_legacy_for_p3".format(listing_id)
+    response = requests.get(api_url)
+    dict_room = json.loads(response.content)
+    room = Room(dict_room["listing"])
+
+    #################### aspect ratings
+    parse_aspect_ratings(room)
+
+    #################### comments
+    room.comments = parse_comments(listing_id)
+
+    return room
+
+
+
