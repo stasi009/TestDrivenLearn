f06aef6d10ba849fd042738bf4a56f98824ce9a9
Stasi <stasi009@163.com>
2017-04-04 12:19:07 +0800

    two more tests

---
+++ scala/playscala/src/tests/OtherTest.scala
@@ -0,0 +1,46 @@
+package tests
+
+import org.scalatest.Spec
+
+class OtherTest extends Spec {
+  
+  def `define multiple val/var in one line` = {
+    val a,b = 9
+    assertResult(9)(a)
+    assertResult(9)(b)
+  }
+  
+  def `conditional expression` = {
+    
+    def __fool(flag: Boolean) = {
+      if (flag) "true" else "false"
+    }
+    
+    assertResult("true")(__fool(true))
+    assertResult("false")(__fool(false))
+  }
+  
+  def `block expression` = {
+    val a = 9
+    val b = {
+      val x = a+1
+      val y = -a
+      x*y
+    }
+    assert(b === -90)
+  }
+  
+  def `test tuples` = {
+    val t = (1,"stasi")
+    
+    // use pattern match to decompose
+    val (id,name) = t
+    assert(id == 1)
+    assert(name == "stasi")
+    
+    // use ._1 or ._2 to access elements
+    assert(t._1 == 1)
+    assert(t._2 == "stasi")
+  }
+
+}
