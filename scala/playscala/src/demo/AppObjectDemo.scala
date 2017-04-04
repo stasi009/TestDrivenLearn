14aea26b6ac5cb7618bb8304150c71635e80bfc4
Stasi <stasi009@163.com>
2017-04-04 11:07:56 +0800

    add more comments

--- scala/playscala/src/demo/AppObjectDemo.scala
+++ scala/playscala/src/demo/AppObjectDemo.scala
@@ -1,5 +1,8 @@
 package demo
 
+/*
+ * execute this App by running: scala -cp bin demo.AppObjectDemo
+ */
 object AppObjectDemo extends App {
   if (args.length > 0)
     println("hello, " + args(0))
