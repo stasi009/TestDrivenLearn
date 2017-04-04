43f50d48cbde7964252c69d4cad552351eee0406
Stasi <stasi009@163.com>
2017-04-04 15:36:29 +0800

    more tests

---
+++ scala/playscala/src/tests/CaseClassTest.scala
@@ -0,0 +1,86 @@
+package tests
+
+import org.scalatest.Spec
+
+class CaseClassTest extends Spec {
+
+  object `case class polymorphism` {
+
+    /*
+     * declare the parent class to be sealed
+     * then the compiler can check "match completeness" for us
+     */
+    sealed trait OsObject
+    case class File(name: String) extends OsObject
+    case class Process(name: String, priority: Int) extends OsObject
+    case object Unknown extends OsObject
+
+    def getName(osobj: OsObject) = {
+      osobj match {
+        case f @ File(name) => name // 'f' represents the "file" object
+        case p @ Process(name, priority) => name // 'p' represents the "process" object
+        case Unknown => ""
+      }
+    }
+
+    def `demo 1` = {
+      val osobjects = Seq(File("a.txt"), Process("run", 1), Unknown)
+      val names = osobjects map getName
+      assert(names == Seq("a.txt", "run", ""))
+    } //def
+
+  } //object
+
+  object `demo usage` {
+    case class Fool(number: Int, name: String)
+
+    // demo that "case class" can also extends trait or abstract classes
+    case class Item(v: Int) extends Ordered[Item] {
+      override def compare(other: Item) = v - other.v
+    } //class
+
+    def `companion object apply` = {
+      val f1 = Fool(1, "abc")
+      assertResult(1)(f1.number)
+
+      val f2 = Fool(name = "cd", number = 9) // use named arguments
+      assertResult("cd")(f2.name)
+    }
+
+    def `companion object unapply` = {
+      val Fool(number, name) = Fool(1, "xy")
+      assert(number == 1)
+      assert(name == "xy")
+    }
+
+    def `auto value-based equality check` = {
+      val f1 = Fool(1, "abc")
+      val f2 = Fool(1, "abc")
+
+      assert(!(f1 eq f2))
+      assert(f1 == f2)
+    }
+
+    def `test toString` = {
+      val f = Fool(1, "abc")
+      assertResult("Fool(1,abc)")(f.toString)
+    }
+
+    def `use copy to modify` = {
+      val f1 = Fool(1, "abc")
+      val f2 = f1.copy(number = 99)
+
+      assert(f2 == Fool(99, "abc"))
+      assert(f1 == Fool(1, "abc")) // f1 remains the same
+    }
+    
+    def `demo case class can extend` = {
+      val numbers = Seq(1,9,8,4)
+      val balls = numbers map {Item(_)}
+      
+      val expected = numbers.sorted map{Item(_)}
+      assert(balls.sorted == expected)
+    }//def
+  } //object
+
+}
---
+++ scala/playscala/src/tests/ExtractorTest.scala
@@ -0,0 +1,102 @@
+package tests
+
+import org.scalatest.Spec
+
+sealed class ExtractorTest extends Spec {
+
+  object Student {
+    def apply(id: Int, name: String, sex: String, addr: String) = s"$id,$name,$sex,$addr"
+
+    def unapply(msg: String) = {
+      val parts = msg.split(",")
+      if (parts.length == 4) Some(parts(0).toInt, parts(1), parts(2), parts(3))
+      else None
+    } //def
+  } //extractor object
+
+  object IsMale {
+    // return Boolean to indicate "matched or not"
+    def unapply(s: String) = s.toLowerCase == "male"
+  }
+
+  object Name {
+    def apply(firstname: String, lastname: String) = s"$firstname $lastname"
+
+    def unapply(fullname: String) = {
+      val segments = fullname split " "
+      if (segments.length == 2) Some(segments(0), segments(1))
+      else None
+    }
+  }
+
+  def `injection method` = {
+    // ------------- inject
+    val firstname = "stasi"
+    val lastname = "kgb"
+    val fullname = Name(firstname, lastname)
+
+    // ------------- extract
+    val Name(cpyfirst, cpylast) = fullname
+    assert(cpyfirst == firstname)
+    assert(cpylast == lastname)
+  }
+
+  def `demo 1` = {
+    val Name(firstname, lastname) = "stasi cheka"
+    assert(firstname == "stasi")
+    assert(lastname == "cheka")
+
+    intercept[MatchError] { val Name(_, _) = "stasi" }
+  }
+
+  def `used in pattern match` = {
+    def isValidName(name: String) = {
+      name match {
+        case Name(_, _) => true
+        case _ => false
+      } //match
+    } //def
+    assertResult(Seq(true, false, true)) { Seq("stasi kgb", "x", "gru cheka") map isValidName }
+  }
+
+  // it will automatically filter out those not matched
+  def `for loop will ignore not-matched` = {
+    val students = Seq(
+      "11,Justin,male,Kaohsiung",
+      "yyyyyyyyyyyyy",
+      "98,Monica,female,Kaohsiung",
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      "66,Bush,male,Taipei")
+    val ids = for (Student(id, name, sex, addr) <- students) yield id
+    assert(ids == Seq(11, 98, 66))
+  } //def
+
+  def `use extractor to judge matched or not` = {
+    val students = Seq(
+      "11,Justin,male,Kaohsiung",
+      "yyyyyyyyyyyyy",
+      "98,Monica,female,Kaohsiung",
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      "66,Bush,male,Taipei")
+
+    val ids = for (Student(id, _, sex @ IsMale(), _) <- students) yield id
+    assert(ids == Seq(11, 66))
+  } //def
+
+  def `use in partial function` = {
+    val students = Seq(
+      "11,Justin,male,Kaohsiung",
+      "yyyyyyyyyyyyy",
+      "98,Monica,female,Kaohsiung",
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+      "66,Bush,male,Taipei")
+
+    // partial function, ignore those not matched
+    val allids = students collect { case Student(id, _, _, _) => id }
+    assert(allids == Seq(11, 98, 66))
+
+    val maleids = students collect { case Student(id, _, sex @ IsMale(), _) => id }
+    assert(maleids == Seq(11, 66))
+  }
+
+}
