f0c2f01cd26b5acbc9eb1ce597da4a95770c875d
Stasi <stasi009@163.com>
2017-04-18 10:24:24 +0800

    we should use raw_input in python2.7

--- Python/TestPython2/demo_print_input.py
+++ Python/TestPython2/demo_print_input.py
@@ -6,28 +6,28 @@ def demo_print_multiples():
     d = True
     # print multiple items on one line, seperated by whitespace
     print(a,b,c,d)
-    
+
 def demo_howto_print_oneline():
     ################### print in multiple lines
     for c in ["a","b","c"]:
         print(c)
-        
+
     ################### print in single line
-    for c in ["a","b","c"]:            
-        print(c,end="")    
-    
-# only the specific character used to quote the string needs to be escaped. 
+    for c in ["a","b","c"]:
+        print(c,end="")
+
+# only the specific character used to quote the string needs to be escaped.
 # so in a single-quoted string, double-quote is not necessary to escape
 def demo_escape():
     print ("So I said, \"You don't know me! You'll never understand me!\"")
     print ('So I said, "You don\'t know me! You\'ll never understand me!"')
     print ("This will result in only three backslashes: \\ \\ \\")
-    print ("""The double quotation mark (") is used to indicate direct quotations.""")    
-    
+    print ("""The double quotation mark (") is used to indicate direct quotations.""")
+
 def demo_input_string():
-    userinput = input("please input: ")
+    userinput = raw_input("please input: ")
     print("your input is: " + userinput)
-    
+
 def demo_input_numbers():
     num1 = int (input("give me a number: "))
     num2 = int (input("give me another number: "))
@@ -39,4 +39,3 @@ if __name__ == "__main__":
     # demo_input_string()
     # demo_input_numbers()
     demo_howto_print_oneline()
-    
