1387dac947b63d72f089b612341bd829cd48f26a
stasi <stasi009@163.com>
2021-04-23 09:27:46 +0800

    first cpp test

---
+++ CPP/test-cpp/tests/test_stl_vector.cc
@@ -0,0 +1,24 @@
+
+#include <vector>
+#include "gtest/gtest.h"
+
+TEST(StlTest, Constructor)
+{
+    // --------- 初始化一个长度为0的vector
+    std::vector<int> vec1;
+    ASSERT_EQ(vec1.size(), 0);
+
+    // --------- 初始化为指定长度，然后用指定值填充
+    std::vector<int> vec2(3, 8);
+    ASSERT_EQ(vec2.size(), 3);
+    for (int x : vec2)
+    {
+        ASSERT_EQ(x, 8);
+    }
+
+    // --------- initializer_list
+    std::vector<int> vec3 = {11, 22, 33};
+    ASSERT_EQ(vec3[0], 11);
+    ASSERT_EQ(vec3[1], 22);
+    ASSERT_EQ(vec3[2], 33);
+}
